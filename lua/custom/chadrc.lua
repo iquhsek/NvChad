8d1c0a406015f877fa2a531f10f439d042796f74
shuqike <griffinke@berkeley.edu>
2022-05-27 18:30:05 -0700

    GO

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+M.ui = {
+   theme = "one_light",
+}
+
+-- Install plugins
+local userPlugins = require "custom.plugins" -- path to table
+
+M.plugins = {
+}
+
+return M
---
+++ lua/custom/init.lua
@@ -0,0 +1,5 @@
+local map = require("core.utils").map
+
+map("n", "<leader>cc", ":Telescope <CR>")
+map("n", "<C-q>", ":q <CR>")
+
